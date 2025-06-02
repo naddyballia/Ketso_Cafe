85b801880ca99a261a2a489134996dc473106c88
Md Afjal Khan <mdafjalkhan@Mds-MacBook-Air.local>
2025-06-02 22:25:57 +0530

    Pushed all the changes

---
+++ customer-frontend/Item card/scripts.js
@@ -0,0 +1,7 @@
+function runAnimations() {
+  runAnimations_212214046();
+  runAnimations_212214051();
+}
+runAnimations();
+function runAnimations_212214046() {}
+function runAnimations_212214051() {}
